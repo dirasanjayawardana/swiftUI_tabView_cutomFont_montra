daf38f64d05906c32499c3840ed019b238424d8b
Mona Lisa <andreno.juanda11@gmail.com>
2024-04-18 11:34:12 +0700

    Initial Commit

---
+++ MontraDev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MontraDev
+//
+//  Created by MACBOOK PRO on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MontraDev/MontraDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontraDevApp.swift
+//  MontraDev
+//
+//  Created by MACBOOK PRO on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MontraDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
